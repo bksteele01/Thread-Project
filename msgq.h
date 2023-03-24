c59cd9ec5fec9a4ab658bb6d0a7cdd264b07b330
bksteele01 <bksteele01@gmail.com>
2023-03-24 12:49:40 -0400

    init

---
+++ msgq.h
@@ -0,0 +1,5 @@
+struct msgq *msgq_init(int num_msgs);
+int msgq_send(struct msgq *mq, char *msg);
+char *msgq_recv(struct msgq *mq);
+int msgq_len(struct msgq *mq);
+void msgq_show(struct msgq *mq);
